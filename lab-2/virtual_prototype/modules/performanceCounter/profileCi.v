040cb6c6d957f522beeb1aa75b913ce2bd865b2b
giorgioajmone <giorgioajmone@protonmail.com>
2024-03-13 19:15:20 +0100

    lab-2

--- lab-2/virtual_prototype/modules/performanceCounter/profileCi.v
+++ lab-2/virtual_prototype/modules/performanceCounter/profileCi.v
@@ -17,7 +17,7 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter0 (
         .clock(clock),
-        .reset(reset || stableB[8]),
+        .reset(reset || valueB[8]),
         .enable(en0),
         .direction(1'b1),
         .counterValue(counterValue0)
@@ -25,7 +25,7 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter1 (
         .clock(clock),
-        .reset(reset || stableB[9]),
+        .reset(reset || valueB[9]),
         .enable(stall && en1),
         .direction(1'b1),
         .counterValue(counterValue1)
@@ -33,7 +33,7 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter2 (
         .clock(clock),
-        .reset(reset || stableB[10]),
+        .reset(reset || valueB[10]),
         .enable(busIdle && en2),
         .direction(1'b1),
         .counterValue(counterValue2)
@@ -41,7 +41,7 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter3 (
         .clock(clock),
-        .reset(reset || stableB[11]),
+        .reset(reset || valueB[11]),
         .enable(en3),
         .direction(1'b1),
         .counterValue(counterValue3)
@@ -56,10 +56,10 @@ module profileCi #(parameter[7:0] customId = 8'h00)
             en0 <= 0; en1 <= 0; en2 <= 0; en3 <= 0;
         end else begin
             if(cIn == customId && start == 1'b1) begin
-                en0 <= (valueB[4] == 1) ? 0 : (valueB[0] == 1) : 1 : en0;
-                en1 <= (valueB[5] == 1) ? 0 : (valueB[1] == 1) : 1 : en1;
-                en0 <= (valueB[6] == 1) ? 0 : (valueB[2] == 1) : 1 : en2;
-                en0 <= (valueB[7] == 1) ? 0 : (valueB[3] == 1) : 1 : en3;
+                en0 <= (valueB[4] == 1'b1) ? 0 : (valueB[0] == 1'b1) ? 1 : en0;
+                en1 <= (valueB[5] == 1'b1) ? 0 : (valueB[1] == 1'b1) ? 1 : en1;
+                en0 <= (valueB[6] == 1'b1) ? 0 : (valueB[2] == 1'b1) ? 1 : en2;
+                en0 <= (valueB[7] == 1'b1) ? 0 : (valueB[3] == 1'b1) ? 1 : en3;
             end
         end
     end
