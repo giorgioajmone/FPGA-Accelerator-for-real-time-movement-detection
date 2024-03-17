ce3946159cf91b5b1b51129f0a689714c8de1b74
Alessandro Cardinale <acardina@ic-24MA-spr-007.intranet.epfl.ch>
2024-03-17 20:10:57 +0100

    profileCi version 17-03

--- lab-2/virtual_prototype/modules/performanceCounter/profileCi.v
+++ lab-2/virtual_prototype/modules/performanceCounter/profileCi.v
@@ -7,6 +7,9 @@ module profileCi #(parameter[7:0] customId = 8'h00)
         output wire[31:0] result
     );
 
+    wire instruction_true;
+    assign instruction_true = cIn == customId && start == 1'b1;
+
     wire [31:0] counterValue0;
     wire [31:0] counterValue1;
     wire [31:0] counterValue2;
@@ -17,7 +20,7 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter0 (
         .clock(clock),
-        .reset(reset || (valueB[8] && cIn == customId && start == 1'b1)),
+        .reset(reset || (valueB[8] && instruction_true)),
         .enable(en0),
         .direction(1'b1),
         .counterValue(counterValue0)
@@ -25,7 +28,7 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter1 (
         .clock(clock),
-        .reset(reset || (valueB[9] && cIn == customId && start == 1'b1)),
+        .reset(reset || (valueB[9] && instruction_true)),
         .enable(stall && en1),
         .direction(1'b1),
         .counterValue(counterValue1)
@@ -33,7 +36,7 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter2 (
         .clock(clock),
-        .reset(reset || (valueB[10] && cIn == customId && start == 1'b1)),
+        .reset(reset || (valueB[10] && instruction_true)),
         .enable(busIdle && en2),
         .direction(1'b1),
         .counterValue(counterValue2)
@@ -41,15 +44,17 @@ module profileCi #(parameter[7:0] customId = 8'h00)
 
     counter #(.WIDTH(32)) counter3 (
         .clock(clock),
-        .reset(reset || (valueB[11] && cIn == customId && start == 1'b1)),
+        .reset(reset || (valueB[11] && instruction_true)),
         .enable(en3),
         .direction(1'b1),
         .counterValue(counterValue3)
     );
 
-    assign result = (cIn != customId || start == 1'b0) ? 32'b0: (valueA[1:0] == 2'd0) ? counterValue0 : (valueA[1:0] == 2'd1) ? counterValue1 : (valueA[1:0] == 2'd2) ? counterValue2 : counterValue3;
+    //assign result = (cIn != customId || start == 1'b0) ? 32'b0: (valueA[1:0] == 2'd0) ? counterValue0 : (valueA[1:0] == 2'd1) ? counterValue1 : (valueA[1:0] == 2'd2) ? counterValue2 : counterValue3;
+    assign result = (instruction_true == 1'b1) ? ((valueA[1:0] == 2'd0) ? counterValue0 : (valueA[1:0] == 2'd1) ? counterValue1 : (valueA[1:0] == 2'd2) ? counterValue2 : counterValue3) : 32'b0;
 
-    assign done = (cIn != customId || start == 1'b0) ? 1'b0 : 1'b1;
+    //assign done = (cIn != customId || start == 1'b0) ? 1'b0 : 1'b1;
+    assign done = (instruction_true == 1'b1) ? 1'b1 : 1'b0;
 
     always @(posedge clock) begin
         if(reset) begin
