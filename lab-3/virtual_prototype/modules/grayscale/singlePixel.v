a73e885c6ba627121a8500f1389a315e8b8b49e6
giorgioajmone <giorgioajmone@protonmail.com>
2024-03-19 14:32:37 +0100

    lab-3

--- lab-3/virtual_prototype/modules/grayscale/singlePixel.v
+++ lab-3/virtual_prototype/modules/grayscale/singlePixel.v
@@ -8,10 +8,10 @@ module singlePixel (
 
 wire[31:0] g_red, g_red01, g_red23, g_red0, g_red1, g_red2, g_red3;
 
-assign g_red0 = (32'h0000001F & red) << 5;
-assign g_red1 = (32'h0000001F & red) << 4;
-assign g_red2 = (32'h0000001F & red) << 2;
-assign g_red3 = (32'h0000001F & red) << 1;
+assign g_red0 = red << 5;
+assign g_red1 = red << 4;
+assign g_red2 = red << 2;
+assign g_red3 = red << 1;
 
 assign g_red01 = g_red0 + g_red1;
 assign g_red23 = g_red2 + g_red3;
@@ -22,10 +22,10 @@ assign g_red = (g_red01 + g_red23) >> 5;
 
 wire[31:0] g_green, g_green01, g_green23, g_green0, g_green1, g_green2, g_green3;
 
-assign g_green0 = (32'h0000003F & green) << 7;
-assign g_green1 = (32'h0000003F & green) << 6;
-assign g_green2 = (32'h0000003F & green) << 3;
-assign g_green3 = (32'h0000003F & green);
+assign g_green0 = green << 7;
+assign g_green1 = green << 6;
+assign g_green2 = green << 3;
+assign g_green3 = green;
 
 assign g_green01 = g_green0 + g_green1;
 assign g_green23 = g_green2 + g_green3;
@@ -36,9 +36,9 @@ assign g_green = (g_green01 - g_green23) >> 6;
 
 wire[31:0] g_blue, g_blue01, g_blue0, g_blue1, g_blue2, g_blue3;
 
-assign g_blue0 = (32'h0000001F & blue) << 4;
-assign g_blue1 = (32'h0000001F & blue) << 1;
-assign g_blue2 = (32'h0000001F & blue);
+assign g_blue0 = blue << 4;
+assign g_blue1 = blue << 1;
+assign g_blue2 = blue;
 
 assign g_blue01 = g_blue0 + g_blue1;
 
