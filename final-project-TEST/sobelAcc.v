eb49be50ba095f4ebf260bfc5b5d5da41608ec9e
AleCardinale <alessandro.cardinale21@gmail.com>
2024-05-07 16:25:09 +0200

    non ho rotto tutto (per ora)

--- final-project-TEST/sobelAcc.v
+++ final-project-TEST/sobelAcc.v
@@ -94,155 +94,164 @@ module sobelAcc #(parameter [7:0] customId = 8'd0) (
 
     wire overwrite = ~RowCount[0];   // overwrite at each new triplet or rows. active low signal
 
-    assign writeData[0] = (readData[0] & overwrite) + camData * coefficient[0];
-    assign writeData[1] = (readData[1] & overwrite) + camData * coefficient[1];    
-    assign writeData[2] = (readData[2] & overwrite) + camData * coefficient[2];
-    assign writeData[3] = (readData[3] & overwrite) + camData * coefficient[3];
-    assign writeData[4] = (readData[4] & overwrite) + camData * coefficient[4];
-    assign writeData[5] = (readData[5] & overwrite) + camData * coefficient[5];
-    assign writeData[6] = (readData[6] & overwrite) + camData * coefficient[6];
-    assign writeData[7] = (readData[7] & overwrite) + camData * coefficient[7];
-    assign writeData[8] = (readData[8] & overwrite) + camData * coefficient[8];
+    wire[15:0] filteredData[0:8];
+
+    assign writeData[0] = readData[0] + filteredData[0];
+    assign writeData[1] = readData[1] + filteredData[1];    
+    assign writeData[2] = readData[2] + filteredData[2];
+    assign writeData[3] = readData[3] + filteredData[3];
+    assign writeData[4] = readData[4] + filteredData[4];
+    assign writeData[5] = readData[5] + filteredData[5];
+    assign writeData[6] = readData[6] + filteredData[6];
+    assign writeData[7] = readData[7] + filteredData[7];
+    assign writeData[8] = readData[8] + filteredData[8];
+
+    // Compute in advance all the possible results and then multiplex them for every memory
+
+    wire[15:0] resultx1  = camData;
+    wire[15:0] resultx2  = camData << 1;
+    wire[15:0] resultx_1 = ~camData + 1;
+    wire[15:0] resultx_2 = ~(camData << 1) + 1; 
 
 
-    // Chose the coefficient of the Sobel filter according to the Row and Column of the pixel: hard-code the logic to each memory block
+    // Chose the filteredData of the Sobel filter according to the Row and Column of the pixel: hard-code the logic to each memory block
 
     // X filtering
 
-    reg[2:0] coefficient[0:8];  // 3 bits to support the 2's complement
+    reg[2:0] filteredData[0:8];  // 3 bits to support the 2's complement
 
     // MEM 0
     always @* begin
         if((RowCount[0] & pixelCount[0]) | RowCount[2] & pixelCount[0]) begin
-                coefficient[0] = -1;
+                filteredData[0] = resultx_1;
         end else if((RowCount[0] & pixelCount[2]) | (RowCount[2] & pixelCount[2])) begin
-                coefficient[0] = 3'd1;
+                filteredData[0] = resultx1;
         end else if(RowCount[1] & pixelCount[0]) begin
-                coefficient[0] = -2;
+                filteredData[0] = resultx_2;
         end else if(RowCount[1] & pixelCount[2]) begin
-                coefficient[0] = 3'd2;
+                filteredData[0] = resultx2;
         end else begin
-                coefficient[0] = 3'd0;
+                filteredData[0] = 16'd0;
         end
     end
 
     // MEM 1
     always @(*) begin
         if((RowCount[0] & pixelCount[1]) | RowCount[2] & pixelCount[1]) begin
-                coefficient[1] = -1;
+                filteredData[1] = resultx_1;
         end else if((RowCount[0] & pixelCount[0]) | (RowCount[2] & pixelCount[0])) begin
-                coefficient[1] = 3'd1;
+                filteredData[1] = resultx1;
         end else if(RowCount[1] & pixelCount[1]) begin
-                coefficient[1] = -2;
+                filteredData[1] = resultx_2;
         end else if(RowCount[1] & pixelCount[0]) begin
-                coefficient[1] = 3'd2;
+                filteredData[1] = resultx2;
         end else begin
-                coefficient[1] = 3'd0;
+                filteredData[1] = 16'd0;
         end        
     end
 
     // MEM 2
     always @(*) begin
         if((RowCount[0] & pixelCount[2]) | RowCount[2] & pixelCount[2]) begin
-                coefficient[2] = -1;
+                filteredData[2] = resultx_1;
         end else if((RowCount[0] & pixelCount[1]) | (RowCount[2] & pixelCount[1])) begin
-                coefficient[2] = 3'd1;
+                filteredData[2] = resultx1;
         end else if(RowCount[1] & pixelCount[2]) begin
-                coefficient[2] = -2;
+                filteredData[2] = resultx_2;
         end else if(RowCount[1] & pixelCount[1]) begin
-                coefficient[2] = 3'd2;
+                filteredData[2] = resultx2;
         end else begin
-                coefficient[1] = 3'd0;
+                filteredData[1] = 16'd0;
         end        
     end
 
     // MEM 3
     always @* begin
         if((RowCount[1] & pixelCount[0]) | RowCount[0] & pixelCount[0]) begin
-                coefficient[3] = -1;
+                filteredData[3] = resultx_1;
         end else if((RowCount[1] & pixelCount[2]) | (RowCount[0] & pixelCount[2])) begin
-                coefficient[3] = 3'd1;
+                filteredData[3] = resultx1;
         end else if(RowCount[2] & pixelCount[0]) begin
-                coefficient[3] = -2;
+                filteredData[3] = resultx_2;
         end else if(RowCount[2] & pixelCount[2]) begin
-                coefficient[3] = 3'd2;
+                filteredData[3] = resultx2;
         end else begin
-                coefficient[3] = 3'd0;
+                filteredData[3] = 16'd0;
         end
     end
 
     // MEM 4
     always @(*) begin
         if((RowCount[1] & pixelCount[1]) | RowCount[0] & pixelCount[1]) begin
-                coefficient[4] = -1;
+                filteredData[4] = resultx_1;
         end else if((RowCount[1] & pixelCount[0]) | (RowCount[0] & pixelCount[0])) begin
-                coefficient[4] = 3'd1;
+                filteredData[4] = resultx1;
         end else if(RowCount[2] & pixelCount[1]) begin
-                coefficient[4] = -2;
+                filteredData[4] = resultx_2;
         end else if(RowCount[2] & pixelCount[0]) begin
-                coefficient[4] = 3'd2;
+                filteredData[4] = resultx2;
         end else begin
-                coefficient[4] = 3'd0;
+                filteredData[4] = 16'd0;
         end        
     end
 
     // MEM 5
     always @(*) begin
         if((RowCount[1] & pixelCount[2]) | RowCount[0] & pixelCount[2]) begin
-                coefficient[5] = -1;
+                filteredData[5] = resultx_1;
         end else if((RowCount[1] & pixelCount[1]) | (RowCount[0] & pixelCount[1])) begin
-                coefficient[5] = 3'd1;
+                filteredData[5] = resultx1;
         end else if(RowCount[2] & pixelCount[2]) begin
-                coefficient[5] = -2;
+                filteredData[5] = resultx_2;
         end else if(RowCount[2] & pixelCount[1]) begin
-                coefficient[5] = 3'd2;
+                filteredData[5] = resultx2;
         end else begin
-                coefficient[5] = 3'd0;
+                filteredData[5] = 16'd0;
         end        
     end
 
     // MEM 6
     always @* begin
         if((RowCount[2] & pixelCount[0]) | RowCount[1] & pixelCount[0]) begin
-                coefficient[6] = -1;
+                filteredData[6] = resultx_1;
         end else if((RowCount[2] & pixelCount[2]) | (RowCount[1] & pixelCount[2])) begin
-                coefficient[6] = 3'd1;
+                filteredData[6] = resultx1;
         end else if(RowCount[0] & pixelCount[0]) begin
-                coefficient[6] = -2;
+                filteredData[6] = resultx_2;
         end else if(RowCount[0] & pixelCount[2]) begin
-                coefficient[6] = 3'd2;
+                filteredData[6] = resultx2;
         end else begin
-                coefficient[6] = 3'd0;
+                filteredData[6] = 16'd0;
         end
     end
 
     // MEM 7
     always @(*) begin
         if((RowCount[2] & pixelCount[1]) | RowCount[1] & pixelCount[1]) begin
-                coefficient[7] = -1;
+                filteredData[7] = resultx_1;
         end else if((RowCount[2] & pixelCount[0]) | (RowCount[1] & pixelCount[0])) begin
-                coefficient[7] = 3'd1;
+                filteredData[7] = resultx1;
         end else if(RowCount[0] & pixelCount[1]) begin
-                coefficient[7] = -2;
+                filteredData[7] = resultx_2;
         end else if(RowCount[0] & pixelCount[0]) begin
-                coefficient[7] = 3'd2;
+                filteredData[7] = resultx2;
         end else begin
-                coefficient[7] = 3'd0;
+                filteredData[7] = 16'd0;
         end        
     end
 
     // MEM 8
     always @(*) begin
         if((RowCount[2] & pixelCount[2]) | RowCount[1] & pixelCount[2]) begin
-                coefficient[8] = -1;
+                filteredData[8] = resultx_1;
         end else if((RowCount[2] & pixelCount[1]) | (RowCount[1] & pixelCount[1])) begin
-                coefficient[8] = 3'd1;
+                filteredData[8] = resultx1;
         end else if(RowCount[0] & pixelCount[2]) begin
-                coefficient[8] = -2;
+                filteredData[8] = resultx_2;
         end else if(RowCount[0] & pixelCount[1]) begin
-                coefficient[8] = 3'd2;
+                filteredData[8] = resultx2;
         end else begin
-                coefficient[8] = 3'd0;
+                filteredData[8] = 16'd0;
         end        
     end
 
